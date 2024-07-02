f92c111f80fb1c2788fe205d53b90b806565728e
Vaibhav Diwan <divaibhav@gmail.com>
2024-07-02 13:36:03 +0530

    day6 codes

---
+++ day6-datatype/variables.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    //declaring an integer varibale
+    //datatype identifier;
+    int marks;
+   
+    //initialize the variable
+    //varibale name = value;
+    marks = 25;
+    cout << "The value of marks = ";
+    cout << marks;
+}
