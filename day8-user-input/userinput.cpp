6a033aa58a5ee8f8fd9125dc345fe3ff824b2538
Vaibhav Diwan <divaibhav@gmail.com>
2024-07-04 14:31:45 +0530

    demostrated code on user input

---
+++ day8-user-input/userinput.cpp
@@ -0,0 +1,17 @@
+// to take user input
+
+#include <iostream>
+using namespace std;
+int main(){
+    // a variable
+    int roll_number;
+    // give message to user for input the given value
+    cout << "enter your roll number\n";
+    cin >> roll_number;
+    cout << "Roll number = " << roll_number << "\n";
+    cout << "enter your age\n";
+    float age;
+    cin >> age;
+    cout << "User Age = " << age << "\n";
+    return 0;
+}
---
+++ day8-user-input/userinputsum.cpp
@@ -0,0 +1,14 @@
+//WAP to accept two integer values from user and
+// print there  sum on the console.
+#include <iostream>
+using namespace std;
+int main(){
+    int number1;
+    int number2;
+    cout << "enter an integer value\n"; 
+    cin >> number1;
+    cout << "enter an integer value\n";
+    cin >> number2;
+    cout << "Sum = " << number1 + number2;
+    return 0;
+}
