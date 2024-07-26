e59657a81557a7fd6bced34d1b5613c9ea514c93
Vaibhav Diwan <divaibhav@gmail.com>
2024-07-26 13:38:58 +0530

    do while

---
+++ 26july-loops/do_while_demo.cpp
@@ -0,0 +1,26 @@
+// loops
+/*
+    1. do .. while loop
+    2. while loop
+    3. for loop
+
+
+    do while syntax
+    
+    do {
+    
+    //statements;
+    } while (condition) ;
+*/
+#include <iostream>
+using namespace std;
+int main(){
+   char user_choice = 0;
+   do{
+    cout << "Do you want to continue, press  Y or n" << "\n";
+    
+    //cin >> user_choice;
+   } while(1);
+
+   return 0;
+}
